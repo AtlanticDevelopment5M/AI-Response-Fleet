c22db1de3dda12174b22ac1ce3b2615b2dfbdf2b
AtlanticDevelopment5M <austin.cairy@gmail.com>
2025-10-08 09:34:27 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'AI Response Fleet'
+author 'Atlantic Development'
+version '1.0.0'
+description 'Unified Tow + Transport + Coroner with shared config & helpers'
+
+shared_scripts {
+  'shared/config.lua',
+  'shared/utils.lua'
+}
+
+client_scripts {
+  'client/coroner_client.lua',
+  'client/transport_client.lua',
+  'client/tow_client.lua'
+}
+
+server_scripts {
+  'server/main.lua'
+}
+
+dependencies {
+  -- optional: 'ox_target'
+}
 
